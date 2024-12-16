16768a8581a111f6d547718b8fa455c82724ae40
Sergio del Amo <sergio.delamo@softamo.com>
2024-12-16 18:35:58 +0100

    disable in native image

--- http-client-netty-declarative/lib/src/test/java/com/micronautbook/essentials/GetPostsTest.java
+++ http-client-netty-declarative/lib/src/test/java/com/micronautbook/essentials/GetPostsTest.java
@@ -2,12 +2,13 @@ package com.micronautbook.essentials;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest(startApplication = false)
 class GetPostsTest {
 
+    @DisabledInNativeImage
     @Test
     void fetchPosts(PostClient postClient) {
         var posts = postClient.fetchPosts();
